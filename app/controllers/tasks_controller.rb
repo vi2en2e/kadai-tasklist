913c3f27936508a4884bfb714e60aba420655d7e
hiro <mqyu07@yahoo.co.jp>
2019-08-01 19:14:54 +0000

    tasknew

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,20 @@
 class TasksController < ApplicationController
+  
   def index
   	@tasks = Task.all
   end
 
+  
   def show
   	@task = Task.find(params[:id])
   end
 
+  
   def new
     @task = Task.new
   end
 
+  
   def create
     @task = Task.new(task_params)
 
@@ -23,36 +27,34 @@ class TasksController < ApplicationController
     end
   end
 
+  
   def edit
     @task = Task.find(params[:id])
   end
 
+  
   def update
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'タスクが編集されました'
       redirect_to @task
-    else
+    elses
       flash.now[:danger] = 'タスクが編集されませんでした'
       render :new
     end
   end
-  def update
-    @task = Task.find(params[:id])
-
-  if @task.update(task_params)
-    flash[:success] = 'タスクが編集されました'
-    redirect_to @task
-  else
-    flash.now[:danger] = 'タスクが編集されませんでした'
-    render :new
-  end
-  end
-
+  
+  
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'タスクは正常に削除されました'
+    redirect_to tasks_path
   end
 end
+
 
 private
 
